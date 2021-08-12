0a6d952b320133f8442b9fad99451e6b2d2eabe5
김민지 <sense168@naver.com>
2021-08-12 19:17:19 +0900

    [fix] UserService 수정 및 UserController url에 api 추가

--- Backend/sopoong_backend/src/main/java/com/sopoong/controller/UserController.java
+++ Backend/sopoong_backend/src/main/java/com/sopoong/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import com.sopoong.service.UserService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 	
 	@Autowired
