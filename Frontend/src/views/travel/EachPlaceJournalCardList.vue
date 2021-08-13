b782807f895b93ac191a4a7d15a5bb9cbbbd4579
김호석 <hoseok76081024@gmail.com>
2021-08-13 12:03:18 +0900

    [hotfix]수정

--- Frontend/src/views/travel/EachPlaceJournalCardList.vue
+++ Frontend/src/views/travel/EachPlaceJournalCardList.vue
@@ -41,7 +41,7 @@ export default {
   beforeCreate() {},
   async created() {
     // 기본이 development로 설정되어 있고 , 그 외에도 여러 모드가 있다.
-    if (process.env.NODE_ENV !== "development") {
+    if (process.env.NODE_ENV !== "production") {
       await this.getRandomImages(DEFAULT_IMAGES_COUNT);
     } else {
       await this.getRandomImagesFromLocal();
