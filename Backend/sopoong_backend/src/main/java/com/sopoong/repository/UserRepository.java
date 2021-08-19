c7be753bff8c2d562e844e0f3b1ffa06666fcd60
김호석 <hoseok76081024@gmail.com>
2021-08-19 22:28:35 +0900

    [feat] 유저아이디 부분 검색

--- Backend/sopoong_backend/src/main/java/com/sopoong/repository/UserRepository.java
+++ Backend/sopoong_backend/src/main/java/com/sopoong/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.sopoong.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.sopoong.model.entity.Travel;
 import com.sopoong.model.entity.User;
 
 @Repository
@@ -12,7 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	Optional<User> findByUserId(String userId);
 	Optional<User> findByUserNickname(String userNickname);
 	Optional<User> findByUserEmail(String userEmail);
-	
-	Optional<User> findUserByUserEmailAndUserPassword(String userEmail, String userPassword);
-	
+//	
+//	Optional<User> findUserByUserEmailAndUserPassword(String userEmail, String userPassword);
+	Page<User> findByUserIdStartingWith(String id,  Pageable pageable);
 }
