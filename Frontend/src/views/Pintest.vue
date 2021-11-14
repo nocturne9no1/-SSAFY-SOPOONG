28d59b12e0cf7fcd0d9c11b867f5034b1c78f74c
Nocturne9no1 <nocturne9no1@gmail.com>
2021-11-15 01:21:18 +0900

    Del: API key

--- Frontend/src/views/Pintest.vue
+++ Frontend/src/views/Pintest.vue
@@ -1,43 +1,45 @@
 <template>
   <div class="pintest">
-    <GmapMap
-        ref="mapRef"
-        :center="center"
-        :zoom="2"
-        class="google-map"
-    >
+    <GmapMap ref="mapRef" :center="center" :zoom="2" class="google-map">
       <gmap-marker
         :position="marker.position"
         :clickalbe="true"
         :icon="icon"
         @click="clickBasicPin"
       />
-      <gmap-polyline v-bind:path.sync="path" v-bind:options="{ strokeColor:'#008000'}"/>
+      <gmap-polyline
+        v-bind:path.sync="path"
+        v-bind:options="{ strokeColor: '#008000' }"
+      />
     </GmapMap>
-    <div
-      v-if="marker.modal"
-      class="modal"
-      >
+    <div v-if="marker.modal" class="modal">
       <div class="modal-body" v-click-outside="closeModal">
-
         <!-- 사진 들어가는 부분 -->
         <div class="image">
-          <img src="https://picsum.photos/300/500" alt="" class="pic">
+          <img src="https://picsum.photos/300/500" alt="" class="pic" />
           <!-- <img :src="markerData.images[0].url" alt=""> -->
         </div>
-
 
         <!-- 글 작성 부분 -->
         <div class="text">
           <!-- 제목 -->
-          <input type="text" placeholder="Title">
+          <input type="text" placeholder="Title" />
           <!-- 대분류 -->
           <fieldset class="category">
             <legend>Category</legend>
             <div class="category-btns">
-              <button @click="clickBtn('hotel')" class="hotel"><img src="../assets/icon/hotel-icon.png" alt="hotel-icon"></button>
-              <button @click="clickBtn('res')" class="res"><img src="../assets/icon/res-icon.png" alt="res-icon"></button>
-              <button  @click="clickBtn('activity')" class="activity"><img src="../assets/icon/activity-icon.png" alt="activity-icon"></button>
+              <button @click="clickBtn('hotel')" class="hotel">
+                <img src="../assets/icon/hotel-icon.png" alt="hotel-icon" />
+              </button>
+              <button @click="clickBtn('res')" class="res">
+                <img src="../assets/icon/res-icon.png" alt="res-icon" />
+              </button>
+              <button @click="clickBtn('activity')" class="activity">
+                <img
+                  src="../assets/icon/activity-icon.png"
+                  alt="activity-icon"
+                />
+              </button>
             </div>
 
             <!-- 소분류 -->
@@ -83,12 +85,18 @@
               </div>
             </div>
           </fieldset>
-          
 
           <!-- 이동수단 -->
           <fieldset class="transport">
             <legend>transport</legend>
-            <div class="nothing" :class="{'hotel': categoryBind['hotel'], 'res' : categoryBind['res'], 'activity' : categoryBind['activity']}">
+            <div
+              class="nothing"
+              :class="{
+                hotel: categoryBind['hotel'],
+                res: categoryBind['res'],
+                activity: categoryBind['activity'],
+              }"
+            >
               <div class="transport-icons">
                 <span><i class="fas fa-2x fa-plane"></i></span>
                 <span><i class="fas fa-2x fa-subway"></i></span>
@@ -107,19 +115,19 @@
                     <h3>청결</h3>
                   </div>
                   <div class="bars">
-                    <input type="radio" name='rate1' id="rate1-star5" />
+                    <input type="radio" name="rate1" id="rate1-star5" />
                     <label for="rate1-star5"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star4" />
+                    <input type="radio" name="rate1" id="rate1-star4" />
                     <label for="rate1-star4"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star3" />
+                    <input type="radio" name="rate1" id="rate1-star3" />
                     <label for="rate1-star3"></label>
-                  
-                    <input type="radio" name='rate1' id="rate1-star2" />
+
+                    <input type="radio" name="rate1" id="rate1-star2" />
                     <label for="rate1-star2"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star1" />
+                    <input type="radio" name="rate1" id="rate1-star1" />
                     <label for="rate1-star1"></label>
 
                     <div class="text"></div>
@@ -131,43 +139,43 @@
                     <h3>서비스</h3>
                   </class>
                   <div class="bars" id="rate2">
-                    <input type="radio" name='rate2' id="rate2-star5" />
+                    <input type="radio" name="rate2" id="rate2-star5" />
                     <label for="rate2-star5"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star4" />
+                    <input type="radio" name="rate2" id="rate2-star4" />
                     <label for="rate2-star4"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star3" />
+                    <input type="radio" name="rate2" id="rate2-star3" />
                     <label for="rate2-star3"></label>
-                  
-                    <input type="radio" name='rate2' id="rate2-star2" />
+
+                    <input type="radio" name="rate2" id="rate2-star2" />
                     <label for="rate2-star2"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star1" />
+                    <input type="radio" name="rate2" id="rate2-star1" />
                     <label for="rate2-star1"></label>
 
                     <div class="text"></div>
                   </div>
                 </div>
-              
+
                 <div class="rating-system">
                   <div class="rate-name">
                     <h3>위치</h3>
                   </div>
                   <div class="bars">
-                    <input type="radio" name='rate3' id="rate3-star5" />
+                    <input type="radio" name="rate3" id="rate3-star5" />
                     <label for="rate3-star5"></label>
 
-                    <input type="radio" name='rate3' id="rate3-star4" />
+                    <input type="radio" name="rate3" id="rate3-star4" />
                     <label for="rate3-star4"></label>
 
-                    <input type="radio" name='rate3' id="rate3-star3" />
+                    <input type="radio" name="rate3" id="rate3-star3" />
                     <label for="rate3-star3"></label>
-                  
-                    <input type="radio" name='rate3' id="rate3-star2" />
+
+                    <input type="radio" name="rate3" id="rate3-star2" />
                     <label for="rate3-star2"></label>
 
-                    <input type="radio" name='rate3' id="rate3-star1" />
+                    <input type="radio" name="rate3" id="rate3-star1" />
                     <label for="rate3-star1"></label>
 
                     <div class="text"></div>
@@ -175,7 +183,7 @@
                 </div>
               </div>
             </div>
-            
+
             <div v-if="categoryBind['res']" class="rates">
               <div class="res">
                 <div class="rating-system">
@@ -183,19 +191,19 @@
                     <h3>청결</h3>
                   </div>
                   <div class="bars">
-                    <input type="radio" name='rate1' id="rate1-star5" />
+                    <input type="radio" name="rate1" id="rate1-star5" />
                     <label for="rate1-star5"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star4" />
+                    <input type="radio" name="rate1" id="rate1-star4" />
                     <label for="rate1-star4"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star3" />
+                    <input type="radio" name="rate1" id="rate1-star3" />
                     <label for="rate1-star3"></label>
-                  
-                    <input type="radio" name='rate1' id="rate1-star2" />
+
+                    <input type="radio" name="rate1" id="rate1-star2" />
                     <label for="rate1-star2"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star1" />
+                    <input type="radio" name="rate1" id="rate1-star1" />
                     <label for="rate1-star1"></label>
 
                     <div class="text"></div>
@@ -207,43 +215,43 @@
                     <h3>서비스</h3>
                   </class>
                   <div class="bars" id="rate2">
-                    <input type="radio" name='rate2' id="rate2-star5" />
+                    <input type="radio" name="rate2" id="rate2-star5" />
                     <label for="rate2-star5"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star4" />
+                    <input type="radio" name="rate2" id="rate2-star4" />
                     <label for="rate2-star4"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star3" />
+                    <input type="radio" name="rate2" id="rate2-star3" />
                     <label for="rate2-star3"></label>
-                  
-                    <input type="radio" name='rate2' id="rate2-star2" />
+
+                    <input type="radio" name="rate2" id="rate2-star2" />
                     <label for="rate2-star2"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star1" />
+                    <input type="radio" name="rate2" id="rate2-star1" />
                     <label for="rate2-star1"></label>
 
                     <div class="text"></div>
                   </div>
                 </div>
-              
+
                 <div class="rating-system">
                   <div class="rate-name">
                     <h3>맛</h3>
                   </div>
                   <div class="bars">
-                    <input type="radio" name='rate3' id="rate3-star5" />
+                    <input type="radio" name="rate3" id="rate3-star5" />
                     <label for="rate3-star5"></label>
 
-                    <input type="radio" name='rate3' id="rate3-star4" />
+                    <input type="radio" name="rate3" id="rate3-star4" />
                     <label for="rate3-star4"></label>
 
-                    <input type="radio" name='rate3' id="rate3-star3" />
+                    <input type="radio" name="rate3" id="rate3-star3" />
                     <label for="rate3-star3"></label>
-                  
-                    <input type="radio" name='rate3' id="rate3-star2" />
+
+                    <input type="radio" name="rate3" id="rate3-star2" />
                     <label for="rate3-star2"></label>
 
-                    <input type="radio" name='rate3' id="rate3-star1" />
+                    <input type="radio" name="rate3" id="rate3-star1" />
                     <label for="rate3-star1"></label>
 
                     <div class="text"></div>
@@ -259,62 +267,61 @@
                     <h3>위치</h3>
                   </div>
                   <div class="bars">
-                    <input type="radio" name='rate1' id="rate1-star5" />
+                    <input type="radio" name="rate1" id="rate1-star5" />
                     <label for="rate1-star5"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star4" />
+                    <input type="radio" name="rate1" id="rate1-star4" />
                     <label for="rate1-star4"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star3" />
+                    <input type="radio" name="rate1" id="rate1-star3" />
                     <label for="rate1-star3"></label>
-                  
-                    <input type="radio" name='rate1' id="rate1-star2" />
+
+                    <input type="radio" name="rate1" id="rate1-star2" />
                     <label for="rate1-star2"></label>
 
-                    <input type="radio" name='rate1' id="rate1-star1" />
+                    <input type="radio" name="rate1" id="rate1-star1" />
                     <label for="rate1-star1"></label>
 
                     <div class="text"></div>
                   </div>
-                  </div>
+                </div>
 
                 <div class="rating-system">
                   <class class="rate-name">
                     <h3>만족도</h3>
                   </class>
                   <div class="bars" id="rate2">
-                    <input type="radio" name='rate2' id="rate2-star5" />
+                    <input type="radio" name="rate2" id="rate2-star5" />
                     <label for="rate2-star5"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star4" />
+                    <input type="radio" name="rate2" id="rate2-star4" />
                     <label for="rate2-star4"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star3" />
+                    <input type="radio" name="rate2" id="rate2-star3" />
                     <label for="rate2-star3"></label>
-                  
-                    <input type="radio" name='rate2' id="rate2-star2" />
+
+                    <input type="radio" name="rate2" id="rate2-star2" />
                     <label for="rate2-star2"></label>
 
-                    <input type="radio" name='rate2' id="rate2-star1" />
+                    <input type="radio" name="rate2" id="rate2-star1" />
                     <label for="rate2-star1"></label>
 
                     <div class="text"></div>
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
           <!-- 느낀점 -->
           <div class="content">
             <p>Content</p>
             <textarea
-              name="content" id="content"
+              name="content"
+              id="content"
               class="content"
               placeholder="느낀점"
             ></textarea>
           </div>
-
         </div>
       </div>
     </div>
@@ -322,24 +329,23 @@
 </template>
 
 <script>
-import * as VueGoogleMaps from 'vue2-google-maps'
-import Vue from 'vue'
-import '../components/css/pintest.scss'
-import vClickOutside from 'v-click-outside'
+import * as VueGoogleMaps from "vue2-google-maps";
+import Vue from "vue";
+import "../components/css/pintest.scss";
+import vClickOutside from "v-click-outside";
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyBwPv3o2CKXGTbC5qPIjTCdU5F1ToEExio",
+    key: "",
     libraries: "places",
-  }
-})
+  },
+});
 
 export default {
-
-  name: 'eachPin',
+  name: "eachPin",
 
   directives: {
-    clickOutside: vClickOutside.directive
+    clickOutside: vClickOutside.directive,
   },
 
   data() {
@@ -348,10 +354,10 @@ export default {
         lat: 37.5,
         lng: 127,
       },
-      icon:  {
+      icon: {
         url: require("../assets/pins/basicpin.png"),
-        scaledSize: {width: 28, height: 28},
-        labelOrigin: {x: 16, y: -10},
+        scaledSize: { width: 28, height: 28 },
+        labelOrigin: { x: 16, y: -10 },
       },
       marker: {
         position: {
@@ -362,58 +368,60 @@ export default {
       },
       // 데이터 입력 확인용 임시
       markerData: {
-        title: '',
+        title: "",
         category: {
-          main: '',
-          sub: '',
+          main: "",
+          sub: "",
         },
         rates: {
           rate1: 0,
           rate2: 0,
           rate3: 0,
         },
-        comment: '',
+        comment: "",
         images: [
           {
-            url: 'https://www.penguintravel.com/uploads/articleThumb/Th5file_8233.jpg',
-          }
+            url: "https://www.penguintravel.com/uploads/articleThumb/Th5file_8233.jpg",
+          },
         ],
       },
       // 어떤 카테고리 골랐는지
-      whichCategory: '',
+      whichCategory: "",
       // polyline test
       path: [
-            {lat: 55.9352001, lng: -4.7766924 },
-            {lat: 55.9358131, lng: -4.7770143 },
-            {lat: 55.9361256, lng: -4.7767353 },
-            {lat: 37.9366784, lng: -4.7739458 }
+        { lat: 55.9352001, lng: -4.7766924 },
+        { lat: 55.9358131, lng: -4.7770143 },
+        { lat: 55.9361256, lng: -4.7767353 },
+        { lat: 37.9366784, lng: -4.7739458 },
       ],
       category: {
-        main: '',
-        sub: '',
+        main: "",
+        sub: "",
       },
       categoryBind: {
         hotel: false,
         res: false,
         activity: false,
-      }
-    }
+      },
+    };
   },
   methods: {
     clickBasicPin() {
-      this.marker.modal = true
+      this.marker.modal = true;
     },
     closeModal() {
-      this.marker.modal = false
+      this.marker.modal = false;
     },
     clickBtn(category) {
-      this.whichCategory = category
-      this.category.main = category
-      this.categoryBind[category] = true
+      this.whichCategory = category;
+      this.category.main = category;
+      this.categoryBind[category] = true;
       for (let key in this.categoryBind) {
-        if (key != category) { this.categoryBind[key] = false }
+        if (key != category) {
+          this.categoryBind[key] = false;
+        }
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
--- Frontend/src/views/createJourney/CreatePlace.vue
+++ Frontend/src/views/createJourney/CreatePlace.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="pintest">
     <GmapMap
-        ref="mapRef"
-        :center="travel.placeList[0].position"
-        :zoom="14"
-        :options="options"
-        class="google-map"
+      ref="mapRef"
+      :center="travel.placeList[0].position"
+      :zoom="14"
+      :options="options"
+      class="google-map"
     >
       <div v-for="(nowPlace, idx) in travel.placeList" :key="idx">
         <!-- 현재 카테고리에 따른 icon 랜더링 -->
@@ -54,55 +54,47 @@
 
       <!-- polyline component -->
       <div v-for="(eachPath, idx) in paths" :key="idx">
-        <EachPolyline
-          :eachPath="eachPath"
-        />
+        <EachPolyline :eachPath="eachPath" />
       </div>
-      <gmap-polyline v-bind:path.sync="path" v-bind:options="{ strokeColor:'#000000'}"/> 
+      <gmap-polyline
+        v-bind:path.sync="path"
+        v-bind:options="{ strokeColor: '#000000' }"
+      />
     </GmapMap>
-        <!-- Button to go to next step -->
-    <div style="position: absolute; top: 10%;">
+    <!-- Button to go to next step -->
+    <div style="position: absolute; top: 10%">
       <button class="next-btn" @click="onClickNextBtn">Next Step</button>
     </div>
 
-
     <!-- Modal part -->
-    <div
-      v-if="isModalOpen"
-      class="modal"
-      >
+    <div v-if="isModalOpen" class="modal">
       <Modal
         :nowPlace="nowPlace"
         :isModalOpen="isModalOpen"
         @close-modal="closeModal"
       />
-    
     </div>
-
-
-
   </div>
 </template>
 
 <script>
-import * as VueGoogleMaps from 'vue2-google-maps'
-import Vue from 'vue'
-import '../../components/css/createJourney/createplace.scss'
-import vClickOutside from 'v-click-outside'
-import Modal from './Modal.vue'
-import EachPolyline from '../../components/createJourney/Polyline.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
+import Vue from "vue";
+import "../../components/css/createJourney/createplace.scss";
+import vClickOutside from "v-click-outside";
+import Modal from "./Modal.vue";
+import EachPolyline from "../../components/createJourney/Polyline.vue";
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyBwPv3o2CKXGTbC5qPIjTCdU5F1ToEExio",
+    key: "",
     libraries: "places",
-    map_id: '6d451fecd5c294f9',
-  }
-})
+    map_id: "6d451fecd5c294f9",
+  },
+});
 
 export default {
-
-  name: 'createPlace',
+  name: "createPlace",
 
   props: {
     images: Array,
@@ -110,11 +102,12 @@ export default {
   },
 
   components: {
-    Modal, EachPolyline,
+    Modal,
+    EachPolyline,
   },
 
   directives: {
-    clickOutside: vClickOutside.directive
+    clickOutside: vClickOutside.directive,
   },
 
   data() {
@@ -123,30 +116,30 @@ export default {
         lat: 37.5,
         lng: 127,
       },
-      options : {
-        mapId: "6d451fecd5c294f9"
+      options: {
+        mapId: "6d451fecd5c294f9",
       },
       // icon 모음
-      icons:  {
+      icons: {
         basic: {
           url: require("../../assets/pins/basicpin.png"),
-          scaledSize: {width: 28, height: 28},
-          labelOrigin: {x: 16, y: -10},
+          scaledSize: { width: 28, height: 28 },
+          labelOrigin: { x: 16, y: -10 },
         },
         hotel: {
           url: require("../../assets/pins/hotel-pin.png"),
-          scaledSize: {width: 28, height: 28},
-          labelOrigin: {x: 16, y: -10},
+          scaledSize: { width: 28, height: 28 },
+          labelOrigin: { x: 16, y: -10 },
         },
         res: {
           url: require("../../assets/pins/restaurant-pin.png"),
-          scaledSize: {width: 28, height: 28},
-          labelOrigin: {x: 16, y: -10},
+          scaledSize: { width: 28, height: 28 },
+          labelOrigin: { x: 16, y: -10 },
         },
         activity: {
           url: require("../../assets/pins/activity-pin.png"),
-          scaledSize: {width: 28, height: 28},
-          labelOrigin: {x: 16, y: -10},
+          scaledSize: { width: 28, height: 28 },
+          labelOrigin: { x: 16, y: -10 },
         },
       },
       // for marker test
@@ -159,36 +152,36 @@ export default {
       },
       // 데이터 입력 확인용 임시
       markerData: {
-        title: '',
+        title: "",
         category: {
-          main: '',
-          sub: '',
+          main: "",
+          sub: "",
         },
         rates: {
           rate1: 0,
           rate2: 0,
           rate3: 0,
         },
-        comment: '',
+        comment: "",
         images: [
           {
-            url: 'https://www.penguintravel.com/uploads/articleThumb/Th5file_8233.jpg',
-          }
+            url: "https://www.penguintravel.com/uploads/articleThumb/Th5file_8233.jpg",
+          },
         ],
       },
       // 어떤 카테고리 골랐는지
-      whichCategory: '',
+      whichCategory: "",
       // polyline test
       paths: [
-            // {lat: 55.9352001, lng: -4.7766924 },
-            // {lat: 55.9358131, lng: -4.7770143 },
-            // {lat: 55.9361256, lng: -4.7767353 },
-            // {lat: 37.9366784, lng: -4.7739458 }
+        // {lat: 55.9352001, lng: -4.7766924 },
+        // {lat: 55.9358131, lng: -4.7770143 },
+        // {lat: 55.9361256, lng: -4.7767353 },
+        // {lat: 37.9366784, lng: -4.7739458 }
       ],
       path: [],
       category: {
-        main: '',
-        sub: '',
+        main: "",
+        sub: "",
       },
       categoryBind: {
         hotel: false,
@@ -199,19 +192,18 @@ export default {
       nowPlace: {},
       // 모달 열고 닫기용
       isModalOpen: false,
-    }
+    };
   },
 
   // DOM 에 표시 전에 polyline 을 위한 path 데이터 뽑아내야함
   created() {
     // 필요에 따라 (아마도 해야할듯...?) 시간 순으로 images 정렬해야함
     for (let image of this.images) {
-      const position = new Object()
-      position.lat = image.position.lat
-      position.lng = image.position.lng
-      this.path.push(position)
+      const position = new Object();
+      position.lat = image.position.lat;
+      position.lng = image.position.lng;
+      this.path.push(position);
     }
-
   },
 
   // mounted() {
@@ -230,39 +222,41 @@ export default {
   // watch: {
   //   travel: function() {
   //     for (let i=0; i<travel.placeList.length() - 1; i++) {
-        
+
   //     }
   //   },
   // },
 
   methods: {
     openModal(nowPlace) {
-      console.log('asdf')
-      this.nowPlace = nowPlace
-      this.isModalOpen = true
+      console.log("asdf");
+      this.nowPlace = nowPlace;
+      this.isModalOpen = true;
     },
 
     clickBasicPin() {
-      this.marker.modal = true
+      this.marker.modal = true;
     },
 
     closeModal() {
-      this.isModalOpen = false
+      this.isModalOpen = false;
     },
 
     clickBtn(category) {
-      this.whichCategory = category
-      this.category.main = category
-      this.categoryBind[category] = true
+      this.whichCategory = category;
+      this.category.main = category;
+      this.categoryBind[category] = true;
       for (let key in this.categoryBind) {
-        if (key != category) { this.categoryBind[key] = false }
+        if (key != category) {
+          this.categoryBind[key] = false;
+        }
       }
     },
 
     onClickNextBtn() {
-      console.log('asdf')
-      this.$emit('next-step')
+      console.log("asdf");
+      this.$emit("next-step");
     },
-  }
-}
+  },
+};
 </script>
